f3448a01f899a6f0d323f3daa49bdc1620b8e281
okuzumi.keisuke <45667068+taberuhanasu@users.noreply.github.com>
2020-09-09 15:43:38 +0900

    Update アンケート.R

--- アンケート.R
+++ アンケート.R
@@ -182,3 +182,61 @@ fgsingle_bar<-function(dr,dq,da,qNo,
            ggbars+theme+geom_bar(stat="identity",fill=palette)
          }
 fgsingle_bar(dr=d01r,dq=d01q,da=d01a,qNo=1)
+
+
+
+
+#複数回答の作図用関数
+fgmulti<-function(dr,dq,da,qNo,
+                       xord01=NULL,theme=gfbars,palette=cb_palette[1]){
+  #データ抽出
+  d02r<-select(dr,contains(qID[qNo])) #i 番目の回答データ
+  d02q<-dq[qNo,"question"] #i 番目の設問文
+  d02g<-dq[qNo,"graph"] #i 番目のグラフの種類
+  d02_ord<-dq[qNo,"order"] #i 番目の x の並べ方
+  d02a_0<-da[,qID[qNo]] #i 番目の選択肢
+  d02a_0<-d02a_0[d02a_0!=""] #選択肢から空白を削除
+  na01<-length(d02a_0) #選択肢の数をカウント
+  
+  #各選択肢の指摘率
+  d02_p1<-summarise_each(d02r,funs(sum))
+  d02_p0<-summarise_each(d02r,funs(length))
+  prop_1<-d02_p1/d02_p0
+  p02<-data.frame(x=1:na01,
+                  xtext=d02a_0,
+                  freq=t(d02_p1),
+                  prop=t(prop_1))
+  
+  #X 軸の並べ方を指定
+  if(d02_ord=="同"){
+    xord<--p02$aID
+  }else if(d02_ord=="大"){
+    xord<-p02$prop
+    xord[grepl("その他",p02$xtext)]<--1
+  }else{
+    xord<--xord01
+  }
+  
+  #グラフの定義
+  ttl01<-paste(d02q,"\n","（",d02g,"N=",d02_p0[1],"）") #グラフタイトル
+  ggbars<-ggplot(p02, #データを指定
+                    aes(x=reorder(x=xtext,X=xord), #X 軸はxtext をxord の順番で
+                         y=prop))+ #Y 軸はprop のデータを
+    coord_flip()+ #横棒グラフに　
+    scale_y_continuous(labels=scales::percent, #Y 軸の目盛は％表記
+                         limits=c(0,1))+ #Y 軸の範囲は0～1
+    geom_text( #データラベルを記入
+     aes(y=prop, #ラベルの位置
+             label=sprintf("%.1f%%",prop*100)), #prop*100 を、数点以下1 桁で％表記
+       size=4, #ラベルの文字サイズは4
+       hjust=-0.1)+ #ラベルの位置調整、0.1 大きく
+     ggtitle(ttl01)+ #グラフタイトル
+     guides(fill=FALSE) #凡例は非表示
+  
+     #グラフを表示
+     ggbars+theme+geom_bar(stat="identity",fill=palette)
+ }
+
+
+
+fgmulti(dr=d01r,dq=d01q,da=d01a,qNo=05)
